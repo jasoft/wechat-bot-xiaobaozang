bf0d3530a32a6b714e68ace7c8ee13331e4c72a6
John Anders <ja.invent@gmail.com>
2024-05-28 16:10:19 +0800

    move to koa

--- src/common/apiserver.js
+++ src/common/apiserver.js
@@ -1,33 +1,52 @@
-import express from "express"
-import swaggerUi from "swagger-ui-express"
-import swaggerDocument from "./swagger.json" // 这是你的 Swagger 文档
+import Koa from "koa"
+import Router from "koa-router"
+import bodyParser from "koa-bodyparser"
+import { koaSwagger } from "koa2-swagger-ui"
 import { wxclient } from "./wxmessage.js"
 import logger from "./logger.js"
+import path from "path"
+import spec from "./swagger.json"
 // 使用 Swagger UI 中间件来提供 API 文档
 
 export async function startApiServer() {
-	const app = express()
-	app.use(express.json()) // for parsing application/json
+	const app = new Koa()
+	const router = new Router()
+	app.use(bodyParser())
+	app.use(router.routes())
+	app.use(router.allowedMethods())
 
-	app.post("/message", async (req, res) => {
-		const { recipient, message } = req.body
+	router.post("/message", (ctx) => {
+		const { recipient, message } = ctx.request.body
 		if (!recipient || !message) {
-			return res.status(400).json({ error: "Recipient and message are required." })
+			ctx.code = 400
+			ctx.body = { error: "Recipient and message are required." }
 		}
 
 		try {
 			wxclient.sendTxtByName(message, recipient)
-			res.json({ success: true })
+			ctx.body = { message: "Message sent successfully" }
 		} catch (error) {
-			res.status(500).json({ error: "Error sending message." })
+			ctx.code = 500
+			ctx.body = { error: "Error sending message." }
 		}
 	})
-	app.get("/message", async (req, res) => {
+	router.get("/message", (ctx) => {
 		// 实现获取用户的逻辑
-		res.json({ code: 200, message: "一个发送微信的 api" })
+		ctx.body = { code: 200, message: "一个发送微信的 api" }
 	})
+
+	// 使用 Swagger UI 中间件来提供 API 文档
+	app.use(
+		koaSwagger({
+			routePrefix: "/swagger", // host at /swagger instead of default /docs
+			swaggerOptions: {
+				spec: spec,
+			},
+		})
+	)
+
 	app.listen(process.env.API_PORT, () => {
 		logger.info(`API Server is running on port ${process.env.API_PORT}`)
 	})
-	app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+	// 使用 Swagger UI 中间件来提供 API 文档
 }
