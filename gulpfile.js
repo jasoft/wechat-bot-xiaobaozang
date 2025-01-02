64e56437d0033d8b227ec1e6167166615be40c42
Wei Wang <1188540@qq.com>
2025-01-02 21:29:27 +0800

    加入自动重启功能,整合到app里面 不需要单独启动

--- gulpfile.js
+++ gulpfile.js
@@ -2,48 +2,50 @@ const gulp = require("gulp")
 const watch = require("gulp-watch")
 const rsync = require("gulp-rsync")
 const path = require("path")
+const { exec } = require("child_process")
 
 // 定义要监控的目录和排除的目录
 const localDir = "." // 修改为你需要监控的目录
-const remoteDir = "/Volumes/Users/soj/Projects/wechat-bot-xiaobaozang"
+const remoteDir = "Q:\\wechat-bot-xiaobaozang\\"
 const excludeDirs = ["node_modules", "assets", ".git"]
 
 // 构建 rsync 选项
 const rsyncOptions = {
-	root: localDir,
-	destination: remoteDir,
-	archive: true,
-	silent: true,
-	compress: true,
-	recursive: true,
-	relative: true,
-	exclude: excludeDirs,
+  root: localDir,
+  destination: remoteDir,
+  archive: true,
+  silent: true,
+  compress: true,
+  recursive: true,
+  relative: true,
+  exclude: excludeDirs,
 }
 
 // 防抖动函数
 function debounce(func, wait) {
-	let timeout
-	return function (...args) {
-		const context = this
-		clearTimeout(timeout)
-		timeout = setTimeout(() => func.apply(context, args), wait)
-	}
+  let timeout
+  return function (...args) {
+    const context = this
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func.apply(context, args), wait)
+  }
 }
 
 // rsync 同步函数
 const syncFiles = debounce(() => {
-	gulp.src(localDir)
-		.pipe(rsync(rsyncOptions))
-		.on("error", (error) => console.error("Error:", error))
+  const robocopyCmd = `robocopy ${localDir} ${remoteDir} /E /XD ${excludeDirs.join(" ")}`
+  exec(robocopyCmd, (error, stdout, stderr) => {
+    console.log(`Stdout: ${stdout}`)
+  })
 }, 2000) // 设置防抖动等待时间为 2 秒
 
 // 监控文件变化并同步
 gulp.task("watch", () => {
-	watch(
-		[`${localDir}/**/*`, `${localDir}/.env`],
-		{ ignored: excludeDirs.map((dir) => path.join(localDir, dir)), dot: true },
-		syncFiles
-	)
+  watch(
+    [`${localDir}/**/*`, `${localDir}/.env`],
+    { ignored: excludeDirs.map((dir) => path.join(localDir, dir)), dot: true },
+    syncFiles
+  )
 })
 
 // 默认任务
--- src/common/cron/crontab.js
+++ src/common/cron/crontab.js
@@ -7,47 +7,46 @@ const { exec } = require("child_process")
 const prisma = new PrismaClient()
 // 每小时执行一次任务
 export async function startCron(ai) {
-	cron.schedule("*/5 * * * *", () => {
-		logger.info("CRON", "同步聊天记录到Mellisearch")
-		syncChatLogs()
-	})
-	
-	cron.schedule(
-	"*/1 * * * *", () => {
-		logger.info("CRON", "重新启动微信")
-		exec("powershell.exe -File ./autostart.ps1", (error, stdout, stderr) => {
-			if (error) {
-				logger.error("CRON", `执行脚本错误: ${error.message}`)
-				return
-			}
-			if (stderr) {
-				logger.error("CRON", `脚本错误输出: ${stderr}`)
-				return
-			}
-			logger.info("CRON", `脚本输出: ${stdout}`)
-		})
-	})
+  cron.schedule("*/5 * * * *", () => {
+    logger.info("CRON", "同步聊天记录到Mellisearch")
+    syncChatLogs()
+  })
 
-	stopAllCrons()
-	loadCrons(ai)
+  cron.schedule("0 3 * * *", () => {
+    logger.info("CRON", "重新启动微信")
+    exec("powershell.exe -File ./autorestart.ps1", (error, stdout, stderr) => {
+      if (error) {
+        logger.error("CRON", `执行脚本错误: ${error.message}`)
+        return
+      }
+      if (stderr) {
+        logger.error("CRON", `脚本错误输出: ${stderr}`)
+        return
+      }
+      logger.info("CRON", `脚本输出: ${stdout}`)
+    })
+  })
+
+  stopAllCrons()
+  loadCrons(ai)
 }
 let scheduledTasks = []
 async function loadCrons(ai) {
-	const tasks = await prisma.reminder.findMany()
+  const tasks = await prisma.reminder.findMany()
 
-	tasks.forEach((task) => {
-		logger.info("CRON", "加载定时任务", task)
-		scheduledTasks.push(
-			cron.schedule(task.cron, () => {
-				logger.info("CRON", "执行定时任务", task)
-				ai("filehelper", [{ role: "user", content: task.command }]).then((res) => {
-					logger.info("CRON", "执行定时任务结果", res.response)
-				})
-			})
-		)
-	})
+  tasks.forEach((task) => {
+    logger.info("CRON", "加载定时任务", task)
+    scheduledTasks.push(
+      cron.schedule(task.cron, () => {
+        logger.info("CRON", "执行定时任务", task)
+        ai("filehelper", [{ role: "user", content: task.command }]).then((res) => {
+          logger.info("CRON", "执行定时任务结果", res.response)
+        })
+      })
+    )
+  })
 }
 function stopAllCrons() {
-	scheduledTasks.forEach((task) => task.stop())
-	scheduledTasks = [] // 清空任务列表
+  scheduledTasks.forEach((task) => task.stop())
+  scheduledTasks = [] // 清空任务列表
 }
